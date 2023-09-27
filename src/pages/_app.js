493240e6f22919072fc64db85a843aaa04c1e850
Praduman Singh <pradu14jsr@gmail.com>
2023-09-27 20:10:25 +0530

    First Commit

---
+++ src/pages/_app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      
+        <Component {...pageProps} />
+
+    </>
+  );
+}
+
+export default MyApp;
